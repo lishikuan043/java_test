01fe9c3b40751f0232506ec858b8a5eec0810129
lishikuan043 <lishikuan043@users.noreply.github.com>
2018-01-08 16:13:46 +0800

    Add test2.

--- src/Day22_test1.java
+++ src/Day22_test1.java
@@ -3,13 +3,14 @@ import java.io.*;
 /**
  * Created by Administrator on 2018/1/5.
  * 使用文件名称过滤器筛选将指定文件夹下的小于200K的小文件获取并打印。
+ * args:  E:\Java_test\resource
  */
 public class Day22_test1 {
     private static class myFilefilter implements FileFilter{
 
         @Override
         public boolean accept(File pathname) {
-            if (pathname.length() < 200) {
+            if (pathname.length() < 200*1024) {
                 return true;
             }else {
                 return false;
