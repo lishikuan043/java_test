3665ef6c22965d296a0918f147d8a82a8e293ad5
lishikuan043 <lishikuan043@users.noreply.github.com>
2018-03-27 18:35:59 +0800

    Add job test.

--- src/job/niuke_alg/Test2_3_heapsort.java
+++ src/job/niuke_alg/Test2_3_heapsort.java
@@ -3,21 +3,21 @@ package job.niuke_alg;
 public class Test2_3_heapsort {
 
     public static void main(String[] args) {
-        int[] b = {1,2,5,7,3,4,5,6,7};
+        int[] b = {154,35,48,36,27,12,44,44,8,14,26,17,28};
         sort(b);
         for (int i : b) {
-            System.out.print(i);
+            System.out.println(i);
         }
     }
     public static void sort(int[] a)
     {
         int N = a.length-1;
-        for (int k = N/2; k >= 1; k--)
+        for (int k = N/2; k >= 0; k--)
             sink(a, k, N);
-        while (N > 1)
+        while (N > 0)
         {
-            exch(a, 1, N);
-            sink(a, 1, --N);
+            exch(a, 0, N);
+            sink(a, 0, --N);
         }
     }
 
